c74b4242caf5b6926ca560ac60da4fd7bc16b848
S_k_y_Net <maxim.mag@list.ru>
2017-03-08 18:49:52 +0300

    re-create repo

---
+++ TestQA/RunSeleniumDriver/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support;
+
+namespace RunSeleniumDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://japonskie.ru/puzzle/");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+            IWebElement element = driver.FindElement(By.LinkText("Японские кроссворды"));
+            element.Click();
+            Console.Write(element);
+            if (element != null)
+            {
+                element.Click();
+            }
+
+            
+            
+        }
+    }
+}
---
+++ TestQA/UnitTestProject/SeleniumTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+             
+        }
+    }
+}
